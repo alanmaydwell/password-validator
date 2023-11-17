5a95496d4bfa7d219f2be5e8b95a35d3b446a682
alanmaydwell <alan.maydwell@digital.justice.gov.uk>
2023-11-17 15:12:55 +0000

    remove duplicate validate_string_with_validators definition

--- password_validate.py
+++ password_validate.py
@@ -1,13 +1,9 @@
+from support.validators import validate_string_with_validators
 from support.validators import (has_underscore,
                                 has_required_length,
                                 has_capital_letter,
                                 has_lower_case_letter,
                                 has_number)
-
-
-def validate_string_with_validators(mystring: str, validators: list[callable] | tuple[callable]) -> bool:
-    return False not in [v(mystring) for v in validators]
-
 
 def validate_password(password):
     validators = [has_underscore, has_required_length, has_capital_letter, has_lower_case_letter, has_number]
